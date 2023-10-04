4b55689184dd50a5c3df6599d7cb7eec98d054ef
Fran249 <82591984+Fran249@users.noreply.github.com>
2023-10-04 07:16:12 -0300

    mucho por hoy ja jode

---
+++ just-watch-it/tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+        extend: {
+			animation: {
+			  text: 'text 5s ease infinite',
+			},
+			keyframes: {
+			  text: {
+				'0%, 100%': {
+				  'background-size': '200% 200%',
+				  'background-position': 'left center',
+				},
+				'50%': {
+				  'background-size': '200% 200%',
+				  'background-position': 'right center',
+				},
+			  },
+			},
+		  }
+
+  },
+  plugins: [
+	require('flowbite/plugin')
+  ],
+};
