5eb712d5a026dec768e4e86c9bd8630db937fcc8
Fran249 <82591984+Fran249@users.noreply.github.com>
2023-10-05 06:01:22 -0300

    5/10

--- just-watch-it/tailwind.config.cjs
+++ just-watch-it/tailwind.config.cjs
@@ -3,21 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./node_modules/flowbite/**/*.js"],
   theme: {
         extend: {
-			animation: {
-			  text: 'text 5s ease infinite',
-			},
-			keyframes: {
-			  text: {
-				'0%, 100%': {
-				  'background-size': '200% 200%',
-				  'background-position': 'left center',
-				},
-				'50%': {
-				  'background-size': '200% 200%',
-				  'background-position': 'right center',
-				},
-			  },
-			},
+			
 		  }
 
   },
